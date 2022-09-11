f4c6b6a0a9d7ddb6ed169f5caf096e1b9560c6a5
nae_nu <muhammadnaeem39a@gmail.com>
2022-09-11 08:28:18 +0500

    methods in java

---
+++ src/MethodsInJava.java
@@ -0,0 +1,10 @@
+public class MethodsInJava {
+    static void myMethod(int a, int b) {
+      int  sum = a+ b;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        myMethod(4,7);
+    }
+}
