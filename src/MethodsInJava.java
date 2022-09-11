be9fb1ecde39bd120a53c8bd74a365eb9b2e3a12
nae_nu <muhammadnaeem39a@gmail.com>
2022-09-11 08:34:34 +0500

    methods with condition based on  input age from  user

--- src/MethodsInJava.java
+++ src/MethodsInJava.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class MethodsInJava {
-    static void myMethod(int a, int b) {
-      int  sum = a+ b;
-        System.out.println(sum);
+    static void checkAge(int age) {
+
+        // If age is less than 18, print "access denied"
+        if (age < 18) {
+            System.out.println("Access denied - You are not old enough!");
+
+            // If age is greater than, or equal to, 18, print "access granted"
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+
     }
 
     public static void main(String[] args) {
-        myMethod(4,7);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your age to know your access");
+        int age = input.nextInt();
+        checkAge(age);
     }
 }
